a106665d11f6c9f545cc3035f0b72b6da272b727
uvsq22200006 <112813888+uvsq22200006@users.noreply.github.com>
2023-03-18 16:37:19 +0100

    Update plateau_taquin.py

--- plateau_taquin.py
+++ plateau_taquin.py
@@ -1,9 +1,9 @@
 import random
 
 valeurs = list(range(1, 16))
-random.shuffle(numbers)
+random.shuffle(valeurs)
 
-plateau = [[valeurs.pop() for j in range(4)] for i in range(4)]
+plateau = [[valeurs.pop() if valeurs else " " for j in range(4)] for i in range(4)]
 plateau[3][3] = " "
 
 for lignes in plateau:
