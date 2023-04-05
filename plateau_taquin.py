ea68e1c258ce5e810001f765cb466a5b85ff1099
uvsq22104187 <91659835+uvsq22104187@users.noreply.github.com>
2023-04-05 12:47:17 +0200

    Update plateau_taquin.py

--- plateau_taquin.py
+++ plateau_taquin.py
@@ -15,6 +15,7 @@ for lignes in plateau:
 import tkinter as tk
 import random as rd
 
+
 def verif(b):
     # Vérifie si le bouton cliqué est adjacent au bouton0
     pos3 = bouton0.grid_info()
@@ -31,37 +32,60 @@ def verif(b):
     elif (pos4["column"] == pos3["column"] - 1) and (pos4["row"] == pos3["row"]) :
         echange_pos(b)
 
-# fonction qui permet d'échanger la position du bouton cliqué avec le bouton0
-def echange_pos(b):
 
-    # récupère les informations de placement des boutons
+def echange_pos(b):
+    # récupère les informations de placement des widgets
     pos1 = bouton0.grid_info()
     pos2 = b.grid_info()
-    
-    # échange les positions des boutons
-    bouton0.grid(row=pos2['row'], column=pos2['column'])
-    b.grid(row=pos1['row'], column=pos1['column'])
 
-# création de la fenêtre
+    # échange les positions des widgets
+    bouton0.grid(row=pos2["row"], column=pos2["column"])
+    b.grid(row=pos1["row"], column=pos1["column"])
+
+    win(boutons)
+
+
+# renvoie un message si le joueur gagne
+def win(boutons):
+    if ((boutons[0].grid_info()["row"] == 0 and boutons[0].grid_info()["column"] == 0) and 
+        (boutons[1].grid_info()["row"] == 0 and boutons[1].grid_info()["column"] == 1) and
+        (boutons[2].grid_info()["row"] == 0 and boutons[2].grid_info()["column"] == 2) and 
+        (boutons[3].grid_info()["row"] == 0 and boutons[3].grid_info()["column"] == 3) and 
+        (boutons[4].grid_info()["row"] == 1 and boutons[4].grid_info()["column"] == 0) and 
+        (boutons[5].grid_info()["row"] == 1 and boutons[5].grid_info()["column"] == 1) and 
+        (boutons[6].grid_info()["row"] == 1 and boutons[6].grid_info()["column"] == 2) and 
+        (boutons[7].grid_info()["row"] == 1 and boutons[7].grid_info()["column"] == 3) and
+        (boutons[8].grid_info()["row"] == 2 and boutons[8].grid_info()["column"] == 0) and
+        (boutons[9].grid_info()["row"] == 2 and boutons[9].grid_info()["column"] == 1) and
+        (boutons[10].grid_info()["row"] == 2 and boutons[10].grid_info()["column"] == 2) and
+        (boutons[11].grid_info()["row"] == 2 and boutons[11].grid_info()["column"] == 3) and
+        (boutons[12].grid_info()["row"] == 3 and boutons[12].grid_info()["column"] == 0) and
+        (boutons[13].grid_info()["row"] == 3 and boutons[13].grid_info()["column"] == 1) and
+        (boutons[14].grid_info()["row"] == 3 and boutons[14].grid_info()["column"] == 2)):
+        
+        label = tk.Label(fen, text="Vous avez gagnez!", fg = "red2", font = ("helvetica", "30"))
+        label.grid(row=4, column=0, columnspan=4)
+
+
+#création de la fenêtre
 fen = tk.Tk()
 fen.title("Jeu taquin")
 
-# position possible dans la grille
+# création des boutons à des places aléatoire
 pos = [(0,0),(0,1),(0,2),(0,3),(1,0),(1,1),(1,2),(1,3),(2,0),(2,1),(2,2),(2,3),(3,0),(3,1),(3,2)]
 
-# création des boutons sauf le bouton0
+boutons = []
 for i in range(15):
-    row, column = rd.choice(pos) # on choisit les coordonnées parmis "pos"
-    pos.remove((row, column))  # Enlever les coordonnées choisies de la liste des positions restantes
-    bouton = tk.Button(fen, bg = "dark goldenrod", text=f"{i+1}", anchor = "center", height = 3,
-                        width = 7, font = ("helvitica", "20")) # création des boutons et de leur texte
-    bouton.grid(row=row, column=column) # position des boutons
-    bouton.config(command=lambda b=bouton: verif(b)) # commande des boutons qui renvoie à la fonction "echange_pos"
+    row, column = rd.choice(pos)
+    pos.remove((row, column))
+    bouton = tk.Button(fen, bg="dark goldenrod", text=f"{i+1}", anchor="center", height=3,
+                        width=7, font=("helvitica", "20"))
+    bouton.grid(row=row, column=column)
+    bouton.config(command=lambda b=bouton: verif(b))
+    boutons.append(bouton)
 
-# création du bouton0 aux coordonnées (2,2)
-bouton0 = tk.Button(fen, bg = "floral white", anchor = "center", height = 3, width = 7,
-                     font = ("helvitica", "20"))
-bouton0.grid(row = 3, column = 3)
+# création du bouton0
+bouton0 = tk.Button(fen, bg="floral white", height=3, width=7, font=("helvitica", "20"))
+bouton0.grid(row=3, column=3)
 
-# affichage de la fenêtre
 fen.mainloop()
